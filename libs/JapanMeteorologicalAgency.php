559ce951b81a16ac222d4065555e7df2b02c986e
Eiji Kobayashi <eiji@design24c.com>
2021-03-02 17:16:23 +0900

    [modify] JSON処理に変更

--- libs/JapanMeteorologicalAgency.php
+++ libs/JapanMeteorologicalAgency.php
@@ -3,58 +3,54 @@
  * Japan Meteorological Agency
  * Get a weather data from Japan Meteorological Agency website.
  */
-
 date_default_timezone_set ('Asia/Tokyo');
 
-$html = file_get_contents('https://www.jma.go.jp/jp/yoho/346.html');
-//$html = file_get_contents('https://www.jma.go.jp/en/yoho/346.html');
-$html = mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8');
-$dom = new DOMDocument;
-@$dom->loadHTML($html);
-$xpath = new DOMXPath($dom);
+$jma = file_get_contents('https://www.jma.go.jp/bosai/forecast/data/forecast/400000.json');
+$jma = mb_convert_encoding($jma, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+$forecast = json_decode($jma, true);
+
+//echo $forecast[0]['timeSeries'][2]['areas'][0]['temps'][0];
 
 $entries = [];
 $datetime = date('Y-m-d H:i:s');
-$venue = $xpath->evaluate('normalize-space(//table[@class="forecast"]/tr/th[1])', $node);
+$venue = $forecast[0]['timeSeries'][0]['areas'][0]['area']['name'];
 $venue = str_replace('地方', '', $venue);
-$day = $xpath->evaluate('normalize-space(//table[@class="forecast"]/tr[2]/th[1])', $node);
-$day = preg_replace('/[^0-9]/', '', $day);
-$weather = $xpath->evaluate('normalize-space(//table[@class="forecast"]/tr[2]/th[1]/img/@alt)', $node);
+$day = $forecast[0]['reportDatetime'];
+$weather = $forecast[0]['timeSeries'][0]['areas'][0]['weathers'][0];
 $weather_id = '';
 $weather_image = '';
-$icon = $xpath->evaluate('normalize-space(//table[@class="forecast"]/tr[2]/th[1]/img/@src)', $node);
-$icon = preg_replace('/[^0-9]/', '', $icon);
-$temp = $xpath->evaluate('normalize-space(//table[@class="forecast"]/tr[2]/td[3][@class="temp"]/div/table/tr[2]/td[3])', $node);
-$temp = str_replace('度', '', $temp);
+$codes = $forecast[0]['timeSeries'][0]['areas'][0]['weatherCodes'][0];
+$svg = 'https://www.jma.go.jp/bosai/forecast/img/'. $codes .'.svg';
+$temp = $forecast[0]['timeSeries'][2]['areas'][0]['temps'][0];
 
-if ( $icon == '100' || $icon == '700' ) {
+if ( $codes == '100' || $codes == '700' ) {
   $weather_id = '1';
   $weather_image = 'sunny.png';
-} elseif ( $icon == '200' ) {
+} elseif ( $codes == '200' ) {
   $weather_id = '2';
-  $weather_image = 'cloudy.png';
-} elseif ( $icon == '300' || $icon == '308' ) {
+  $weather_image = 'cloudly.png';
+} elseif ( $codes == '300' || $codes == '308' ) {
   $weather_id = '3';
   $weather_image = 'rain.png';
-} elseif ( $icon == '400' || $icon == '406' ) {
+} elseif ( $codes == '400' || $codes == '406' ) {
   $weather_id = '4';
   $weather_image = 'snow.png';
-} elseif ( $icon == '101' || $icon == '110' || $icon == '201' || $icon == '210' || $icon == '701' || $icon == '704' || $icon == '707' || $icon == '710' ) {
+} elseif ( $codes == '101' || $codes == '110' || $codes == '201' || $codes == '210' || $codes == '701' || $codes == '704' || $codes == '707' || $codes == '710' ) {
   $weather_id = '5';
   $weather_image = 'sunny_then_cloudy.png';
-} elseif ( $icon == '102' || $icon == '112' || $icon == '301' || $icon == '311' || $icon == '702' || $icon == '705' || $icon == '708' || $icon == '711' ) {
+} elseif ( $codes == '102' || $codes == '112' || $codes == '301' || $codes == '311' || $codes == '702' || $codes == '705' || $codes == '708' || $codes == '711' ) {
   $weather_id = '6';
   $weather_image = 'sunny_then_rain.png';
-} elseif ( $icon == '104' || $icon == '115' || $icon == '401' || $icon == '411' || $icon == '703' || $icon == '706' || $icon == '709' || $icon == '712' ) {
+} elseif ( $codes == '104' || $codes == '115' || $codes == '401' || $codes == '411' || $codes == '703' || $codes == '706' || $codes == '709' || $codes == '712' ) {
   $weather_id = '7';
   $weather_image = 'sunny_then_snow.png';
-} elseif ( $icon == '202' || $icon == '212' || $icon == '302' || $icon == '313' ) {
+} elseif ( $codes == '202' || $codes == '212' || $codes == '302' || $codes == '313' ) {
   $weather_id = '8';
   $weather_image = 'cloudy_then_rain.png';
-} elseif ( $icon == '204' || $icon == '215' || $icon == '402' || $icon == '413' ) {
+} elseif ( $codes == '204' || $codes == '215' || $codes == '402' || $codes == '413' ) {
   $weather_id = '9';
   $weather_image = 'cloudy_then_snow.png';
-} elseif ( $icon == '303' || $icon == '314' || $icon == '403' || $icon == '414' ) {
+} elseif ( $codes == '303' || $codes == '314' || $codes == '403' || $codes == '414' ) {
   $weather_id = '10';
   $weather_image = 'rain_then_snow.png';
 } else {
@@ -76,38 +72,19 @@ if ( !$temp ) {
 }
 
 $entries[] = [
-  'datetime' => $datetime,
-  'venue'    => $venue,
-  'day'      => $day,
-  'weather'  => $weather,
-  'weather_id'  => $weather_id,
-  'weather_image'  => $weather_image,
-  'icon'     => $icon,
-  'temp'     => $temp,
+  'datetime'      => $datetime,
+  'venue'         => $venue,
+  'day'           => $day,
+  'weather'       => $weather,
+  'weather_id'    => $weather_id,
+  'weather_image' => $weather_image,
+  'codes'         => $codes,
+  'svg'           => $svg,
+  'temp'          => $temp,
 ];
 var_dump($entries);
 
-// 個別にノード取得する処理
-//echo $xpath->evaluate('string(//table[@class="forecast"]/tr/th)', $node);
-//echo $xpath->evaluate('normalize-space(//table[@class="forecast"]/tr/th[1])', $node);
-//echo $xpath->evaluate('string(//table[@class="forecast"]/tr/th[2])', $node);
-//echo $xpath->evaluate('string(//table[@class="forecast"]/tr/th[3])', $node);
-//echo $xpath->evaluate('normalize-space(//table[@class="forecast"]/tr[2]/th[1])', $node);
-//echo $xpath->evaluate('normalize-space(//table[@class="forecast"]/tr[2]/th[1]/img/@alt)', $node);
-//echo $xpath->evaluate('normalize-space(//table[@class="forecast"]/tr[2]/th[1]/img/@src)', $node);
-//echo $xpath->evaluate('normalize-space(//table[@class="forecast"]/tr[2]/td[3][@class="temp"]/div/table/tr[2]/td[3])', $node);
-//echo $xpath->evaluate('string(//table[@class="forecast"]/tr[3]/td[3][@class="temp"]/div/table/tr[2]/td[3])', $node);
-
-// foreach で回してノード取得する処理
-/*foreach ($xpath->query('//table[@class="forecast"]/tr') as $node) {
-  foreach ($xpath->query('.//th', $node) as $tag_node) {
-    echo $tag_node->nodeValue;
-    echo "\n";
-    echo '<br>';
-  }
-}*/
-/*var_dump($entries);*/
-
 $array = json_encode($entries);
-file_put_contents('../assets/json/weather.json' , $array);
+file_put_contents($json_url , $array);
+
 ?>
